15cab552f7fcc64d8cb4cf0f74033fde1e293c2b
Alif <firjatullahalif1310@gmail.com>
2024-02-02 19:14:45 +0700

    first commit

---
+++ latihan3.py
@@ -0,0 +1,7 @@
+print("LATIHAN 3")
+a = float(input("Masukan Nilai ( jika nilai 20-23 atau 34-38 atau 40-42 maka benar) = "))
+
+hasil1 = a > 20 and a < 23, a > 34 and a < 38, a > 40 and a < 42
+
+
+print(hasil1)
